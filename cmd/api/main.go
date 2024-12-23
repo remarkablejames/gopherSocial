ff86f861f397fd37d07474d397d6d060a3836632
James Niyongira <lascasasjames85@gmail.com>
2024-12-23 02:31:42 -0500

    added hot reloading with air-verse

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	"gopherSocial/internal/env"
 	"log"
 )
 
 func main() {
 
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg := config{
-		addr: ":8081",
+		addr: env.GetString("ADDR", ":8081"),
 	}
 	app := &application{
 		config: cfg,
